ea546feb65df6b734948845bddd22a1db243ee35
Riley matewere <emzomatewere@gmail.com>
2023-06-19 23:01:58 +0200

    boat booking system
    boat management system

---
+++ Boat Booking Management System Php/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>BOAT BOOKING SYSTEM FOR SUN AND SAND</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="includes/css/main.css" />
+		<noscript><link rel="stylesheet" href="includes/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.php">BOAT BOOKING SYSTEM/a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="pass-login.php">Make Reservation</a></li>
+								<li><a href="employee/emp-login.php">Employee Login</a></li>
+								<li><a href="admin/emp-login.php">Admin Login</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<div class="logo"><span class="icon solid fa-train"></span></div>
+							<h2>BOAT BOOKING SYSTEM</h2>
+							<p>Best, Simple, and User Friendly Way To Reserve Boat Tickets Effectively</a></p>
+						</div>
+					</section>
+
+					
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="includes/js/jquery.min.js"></script>
+			<script src="includes/js/jquery.scrollex.min.js"></script>
+			<script src="includes/js/browser.min.js"></script>
+			<script src="includes/js/breakpoints.min.js"></script>
+			<script src="includes/js/util.js"></script>
+			<script src="includes/js/main.js"></script>
+
+	</body>
+</html>
